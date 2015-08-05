9e24723c81c391d1f1fa75f94c1fa384bd6f8c61
杨文军 <YWJ@yangwenjun.local>
2015-08-05 19:17:44 +0800

    初始化项目

---
+++ WJRefresh/WJRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WJRefresh
+//
+//  Created by 杨文军 on 15/8/5.
+//  Copyright (c) 2015年 com.it.wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WJRefresh/WJRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJRefresh
+//
+//  Created by 杨文军 on 15/8/5.
+//  Copyright (c) 2015年 com.it.wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
